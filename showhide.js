6b326e9dfc1139f104a01b9cd8e29333b00b3e54
jennybug <jenflorence89@gmail.com>
2020-12-08 12:17:00 +0000

    commit3

---
+++ showhide.js
@@ -0,0 +1,18 @@
+function showhide() {
+    
+    var x = document.getElementById("myform");
+    
+    if(x.style.display==="none") {
+        
+        x.style.display="block";
+    }
+    
+    else {
+        
+        x.style.display="none";
+    }
+    
+    
+    
+    
+}
